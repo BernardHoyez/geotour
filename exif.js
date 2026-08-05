9aeceb2714435286a606c6a5d7d645803522479b
BernardHoyez <bhoyez@gmail.com>
2026-08-05 22:57:06 +0200

    Delete exif.js

--- exif.js
+++
@@ -1,6 +0,0 @@
-// Module partagé : lecture EXIF (parseur binaire custom, sans dépendance).
-// À migrer depuis photo2waypoint.
-//
-// TODO fonctions à fournir :
-// - lireCoordonneesGPS(fileBlob) -> { lat, lon } | null si absent
-// - lireDateHeure(fileBlob) -> Date | null
